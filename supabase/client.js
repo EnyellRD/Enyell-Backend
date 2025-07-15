1e790ad48220b4d8ffbdfc1ac31c31f577a2ccb9
Enyell Valdez <enyell1415@gmail.com>
2025-07-15 02:22:25 -0400

    Versión final con Supabase integrada

--- supabase/client.js
+++ supabase/client.js
@@ -1,1 +1,7 @@
 // Aquí irá la conexión con Supabase
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://hqdoiwvfsaetoqcduvpp.supabase.co'; // <- URL sin salto de línea
+const supabaseKey = 'sb_publishable_pBQevuycbmUky_8z8XcsNA_9awVu31_'; // <- tu Public API Key
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
